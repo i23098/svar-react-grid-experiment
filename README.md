594af51401cc84b603bd409b4383cfd8119fe064
Ricardo Almeida <ric.almeida@gmail.com>
2025-11-25 12:08:52 +0000

    Try to create a table with sample data

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ Attempt to use @svar-ui/react-grid
 `./gradlew build`
 
 Will generate files into `build/dist`, open `index.html` in browser to see the result
+
+## Issue
+
+The table is not rendered properly!
+
+![Table badly rendered](https://github.com/i23098/svar-react-grid-experiment/blob/main/tableRendered.png?raw=true)
--- src/react-js/tableExperiment.tsx
+++ src/react-js/tableExperiment.tsx
@@ -1,5 +1,41 @@
 import React from "react";
 
+import { Grid, Willow } from "@svar-ui/react-grid";
+import "@svar-ui/react-grid/all.css";
+
+import type { IColumnConfig, IRow } from "@svar-ui/react-grid";
+
 export default function TableExperiment() {
-    return <div>Hello World!</div>
+    const columns: IColumnConfig[] = [
+        { id: "city", header: "City" },
+        { id: "description", header: "Description" }
+    ];
+
+    const data: IRow[] = [{
+        "city": "New York",
+        "description": "The city that never sleeps, famous for its landmarks and culture."
+    }, {
+        "city": "London",
+        "description": "A historic global city, home to the Royal Family and iconic double-decker buses."
+    }, {
+        "city": "Tokyo",
+        "description": "A vibrant mix of modern skyscrapers and traditional temples."
+    }, {
+        "city": "Paris",
+        "description": "Known as the 'City of Love' and home to the Eiffel Tower."
+    }, {
+        "city": "Sydney",
+        "description": "Australia's largest city, famous for its Opera House and harbor."
+    }];
+
+    return (
+        <div style={{ width: "80%" }}>
+            <Willow>
+                <Grid
+                    columns={columns}
+                    data={data}
+                />
+            </Willow>
+        </div>
+    );
 }
